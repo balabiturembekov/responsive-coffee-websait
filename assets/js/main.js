4af56c62261133e6e5033cf66f16bd642a400699
Balabi94 <orfejorfeev@gmail.com>
2022-02-18 03:47:45 +0600

    Сверстана секция Products

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,13 +43,27 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
-
+let mixerProducts = mixitup('.products__content', {
+    selectors: {
+        target: '.products__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Default filter products */ 
-
+mixerProducts.filter('.delicacies')
 
 /* Link active products */ 
+const linkProducts = document.querySelectorAll('.products__item')
 
+function activeProducts() {
+    linkProducts.forEach(l => l.classList.remove('active-product'))
+    this.classList.add('active-product')
+}
+
+linkProducts.forEach(l => l.addEventListener('click', activeProducts))
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
